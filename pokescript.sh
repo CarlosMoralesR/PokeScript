ca735142f3e9a73c84e4f0a4ab13dcbbf5c891bd
CarlosMoralesR <carlangetas007@hotmail.com>
2023-01-31 19:39:17 -0600

    PokeScript en bash

---
+++ pokescript.sh
@@ -0,0 +1,23 @@
+#! /usr/bin/bash
+
+if [ $# -eq 0 ]
+then
+	echo "No hay ningún parámetro"
+else
+
+	pokemon=$1
+	if [ "$pokejson" == "Not Found" ]
+	then
+		echo "Nombre de pokemon incorrecto"
+	else
+		# agregar parseo con jq
+		pokejson=$(curl https://pokeapi.co/api/v2/pokemon/$pokemon)
+		id=$(curl https://pokeapi.co/api/v2/pokemon/$pokemon | jq .id)
+		name=$(curl https://pokeapi.co/api/v2/pokemon/$pokemon | jq .name)
+		weight=$(curl https://pokeapi.co/api/v2/pokemon/$pokemon | jq .weight)
+		height=$(curl https://pokeapi.co/api/v2/pokemon/$pokemon | jq .height)
+		order=$(curl https://pokeapi.co/api/v2/pokemon/$pokemon | jq .order)
+		clear
+		echo "Id: $id, name: $name, weight: $weight, height: $height, order: $order"
+	fi
+fi
