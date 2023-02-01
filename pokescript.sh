2e4f177dd43d7d0827462e2e0f7963df2e8a87db
CarlosMoralesR <carlangetas007@hotmail.com>
2023-01-31 20:22:02 -0600

    fixing conditional

--- pokescript.sh
+++ pokescript.sh
@@ -4,14 +4,12 @@ if [ $# -eq 0 ]
 then
 	echo "No hay ningún parámetro"
 else
-
 	pokemon="${1,,?}"
+	pokejson=$(curl https://pokeapi.co/api/v2/pokemon/$pokemon)
 	if [ "$pokejson" == "Not Found" ]
 	then
 		echo "Nombre de pokemon incorrecto"
 	else
-		# agregar parseo con jq
-		pokejson=$(curl https://pokeapi.co/api/v2/pokemon/$pokemon)
 		id=$(curl https://pokeapi.co/api/v2/pokemon/$pokemon | jq .id)
 		name=$(curl https://pokeapi.co/api/v2/pokemon/$pokemon | jq .name)
 		weight=$(curl https://pokeapi.co/api/v2/pokemon/$pokemon | jq .weight)
